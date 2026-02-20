474a8cc38703e22789aa63df807abaef6d8f735f
youssef mohamed <youssefhusseinnn1@gmail.com>
2026-02-20 21:25:46 +0200

    add

--- backend/verify_logs.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('server_final.log', 'utf8');
-const lines = content.split('\n');
-const relevant = lines.filter(l => l.includes('/api/cart') || l.includes('/api/users/login'));
-console.log('Found logs:');
-relevant.forEach(l => console.log(l));
